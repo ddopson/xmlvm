d274b15b8c65aac4ea70575af3f8bc0cd9ce6f60
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-03-24 22:19:56 -0700

    java_lang_Map needs to function as an interface

--- src/xmlvm2objc/compat-lib/objc/java_util_Map.h
+++ src/xmlvm2objc/compat-lib/objc/java_util_Map.h
@@ -26,7 +26,16 @@
 
 // java.util.Map
 //----------------------------------------------------------------------------
-typedef NSDictionary java_util_Map;
+@protocol java_util_Map
+
+- (java_lang_Object*) put___java_lang_Object_java_lang_Object :(java_lang_Object*) key
+                                                              :(java_lang_Object*) value;
+- (java_lang_Object*) get___java_lang_Object :(java_lang_Object*) key;
+- (java_util_Set*) keySet__;
+- (int) size__;
+
+@end
+
 @interface NSDictionary (cat_java_util_Map)
 
 - (java_lang_Object*) put___java_lang_Object_java_lang_Object :(java_lang_Object*) key
