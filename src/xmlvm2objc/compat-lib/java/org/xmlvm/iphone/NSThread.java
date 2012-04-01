0d75060c01c94849d475c0e2de99cb752c2e7b1a
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-04-01 13:31:41 -0700

    org_xmlvm_iphone_NSThread - add [NSThread callStackSymbols]

---
+++ src/xmlvm2objc/compat-lib/java/org/xmlvm/iphone/NSThread.java
@@ -0,0 +1,13 @@
+package org.xmlvm.iphone;
+import java.util.*;
+
+import org.xmlvm.XMLVMSkeletonOnly;
+@XMLVMSkeletonOnly
+public class NSThread extends NSObject {
+	/**
+	 * + (NSArray *)callStackSymbols ;
+	 */
+	public static List callStackSymbols(){
+		throw new RuntimeException("Stub");
+	}
+}
