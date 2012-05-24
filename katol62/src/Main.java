3e8502bd4cbe0b0ebbce0d6b69fcd774d611fef6
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-05-24 09:53:43 -0700

    comment

--- katol62/src/Main.java
+++ katol62/src/Main.java
@@ -1,5 +1,6 @@
 package pkg;
 
+// DO NOT EDIT HERE - This file is only a placeholder.  The generated src gets replaced by objc/pkg_Main.m
 class Main {
   public static void main(String[] args) {
   }
