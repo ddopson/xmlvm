ca656bc464b1cdddf5094e9d2e7ec91028a1b280
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-05-30 09:27:37 -0700

    java test runner

--- katol62/src/Main.java
+++ katol62/src/Main.java
@@ -2,6 +2,20 @@ package pkg;
 
 // DO NOT EDIT HERE - This file is only a placeholder.  The generated src gets replaced by objc/pkg_Main.m
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Running ParseFloat");
+    ParseFloat.testmain(args);
+    
+    System.out.println("Running Extrema");
+    Extrema.testmain(args);
+    
+    System.out.println("Running Constants");
+    Constants.testmain(args);
+
+    System.out.println("Running BitwiseConversion");
+    BitwiseConversion.testmain(args);
+
+    System.out.println("Running NaNInfinityParsing");
+    NaNInfinityParsing.testmain(args);
   }
 }
