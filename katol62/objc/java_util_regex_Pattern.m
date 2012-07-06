d16ac191de0c009771a328e0ba8513b7d3634679
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-07-06 00:22:16 -0700

    cleanup regex class

--- katol62/objc/java_util_regex_Pattern.m
+++ katol62/objc/java_util_regex_Pattern.m
@@ -11,44 +11,30 @@
 @implementation java_util_regex_Pattern
 
 NSRegularExpression *regex;
-java_util_regex_Matcher* matcher;
 
-+(java_util_regex_Pattern*)compile___java_lang_String:(java_lang_String*)str
++(java_util_regex_Pattern*)compile___java_lang_String: (java_lang_String*)str
 {
-    NSAutoreleasePool *loopPool = [[NSAutoreleasePool alloc] init];
-
     java_util_regex_Pattern* pattern = [[[java_util_regex_Pattern alloc] init] autorelease];
     NSError *error = NULL;
     
-//    str = (java_lang_String*)[(NSString *)str stringByReplacingOccurrencesOfString:@"(((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?))|(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))[pP][+-]?(\\p{Digit}+))"
-//                                                     withString:@"(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))[pP][+-]?(\\p{Digit}+))|(((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?))"];
-    
+    // is this needed?
     str = [NSString stringWithFormat:@"^%@$", str];
     
-    pattern->regex = [NSRegularExpression regularExpressionWithPattern:(NSString*)str
+    pattern->regex = [NSRegularExpression regularExpressionWithPattern: (NSString*)str
                                                                 options:0
                                                                 error:&error];
     return pattern;
-    
-    [loopPool drain];
 }
 
--(java_util_regex_Matcher*)matcher___java_lang_CharSequence:(NSString *)chr
+-(java_util_regex_Matcher*)matcher___java_lang_CharSequence: (NSString *)str
 {
-    NSAutoreleasePool *loopPool = [[NSAutoreleasePool alloc] init];
-
-    NSLog(@"chr=%@", chr);
-    NSCharacterSet* whitespace = [NSCharacterSet characterSetWithCharactersInString: @" \t\n\r\f\001\013\037"];
-    NSString* trimmed = [chr stringByTrimmingCharactersInSet:whitespace];
-
-    matcher = [[[java_util_regex_Matcher alloc] init] autorelease];
+    java_util_regex_Matcher* matcher = [[[java_util_regex_Matcher alloc] init] autorelease];
     
-    matcher.matches = [regex matchesInString:trimmed
-                                options:0
-                                range:NSMakeRange(0, [trimmed length])];
+    matcher.matches = [regex matchesInString: str
+                                options: 0
+                                range: NSMakeRange(0, [str length])];
     
     return matcher;
-    [loopPool drain];
 }
 
 @end
